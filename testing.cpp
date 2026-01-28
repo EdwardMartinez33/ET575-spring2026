ac01445cc7e00d9da2e71d76761a18329510f696
EdwardMartinez33 <juniorny33@gmail.com>
2026-01-28 02:16:36 +0000

    lab 2

---
+++ testing.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string n;
+    cout<<"Enter your full name: ";
+    cin>>n;
+    cout<<"Welcome to C++ "<<n<<endl;
+    return 0;
+}
