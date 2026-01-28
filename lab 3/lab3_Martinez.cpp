4a00ec02c77eedd830f7f64e7ab0c1e9f4c92274
EdwardMartinez33 <juniorny33@gmail.com>
2026-01-28 16:59:42 +0000

    class examples lab 3

---
+++ lab 3/lab3_Martinez.cpp
@@ -0,0 +1,35 @@
+/*
+Edward Martinez
+Jan 28 2026
+Lab 3
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"\n ------ Example 1: -------"<<endl;
+    int number = 10.9;
+    float n = 7.5;
+
+    cout<<"number = "<<number<<endl;
+    cout<<"n = "<<n<<endl;
+    
+    cout<<"\n ----- Example 2: Numerical operators -------"<<endl;
+    number = 5;
+    number = 2;
+    cout<<"sum equal to "<<number+n<<endl;
+    cout<<" ";
+    cout<<"Division = "<<number%2<<endl;
+
+    cout<<"\n ------ Example 3: Assignment operators ------"<<endl;
+
+    cout<<"\n ------ Example 4: Comparison operators ------"<<endl;
+
+    cout<<"\n ------ Example 5: Logical operators -------"<<endl;
+    
+    return 0;
+
+
+}
