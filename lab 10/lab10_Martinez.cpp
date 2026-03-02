1bf80dac75477802860efd4c17de87e6d3bd62b4
EdwardMartinez33 <juniorny33@gmail.com>
2026-03-02 16:40:54 +0000

    Lab10_Martinez

---
+++ lab 10/lab10_Martinez.cpp
@@ -0,0 +1,52 @@
+/*
+Edward Martinez
+March 2, 2026
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"\n---- Example 1: for loop basics ----"<<endl;
+
+    for(int action = 0; action+1; action++){
+    cout<<"Hello"<<action<<endl;
+    }
+
+    /* loop analysis table
+    loop iteration | action | actionc5 | cout<<"Hello"<<action<<endl;
+        1              0      true         Hello 0
+        2              1      true         Hello 1
+        3              2      true         Hello 2
+        4              3      true         Hello 3
+        5              4      true         Hello 4 
+        6              5      false        Hello 5
+    */
+   cout<<"\n ---- Example 2: for loop as a increment counter"<<endl;
+   // counter from 1 to 10 (inclusive) with step of 1 -- 1,2,3,4,5,6,7,8,9,10
+   int a = 1;
+   for(a) a(=)10 :;
+
+
+   cout<<"\n ---- Example 3: for loop as a decrement counter"<<endl;
+   // counter from 2 to -5 (inclusive) with step of 1 -->
+   for(int n = 2; n>= -5) n--{
+    cout<<n<<"\t";
+   }
+   cout<<endl;
+   
+   cout<<"\n ---- Example 4: for loop as an increment counter"<<endl;
+   // counter from
+
+   cout<<"\n ---- Example 5: nested conditional statement within the "<<endl;
+   // counter from -10 to 50 step of 3. Count  how many numbers are
+   int counter_5 = 0;
+   for(int N = -10; N<=5)
+
+
+   cout<<"\n ---- EXERCISE 1 ---- "<<endl;
+   for(int)
+
+   cout<<"\n ---- EXERCISE 2 ---- "<<endl;
+    return 0;
+}
