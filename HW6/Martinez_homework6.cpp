b433c8274d1df74d7fdcde1225486504f343494a
EdwardMartinez33 <juniorny33@gmail.com>
2026-03-23 01:00:30 +0000

    HW6

---
+++ HW6/Martinez_homework6.cpp
@@ -0,0 +1,33 @@
+/*
+Edward Martinez
+Homework 6
+March 22, 2026
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"---- Exercise 1 ----"<<endl;
+    int* ptr;
+    int var = 7;
+    int foo = 21;
+    ptr = &var;
+    ptr = &foo;
+    int& ref = var;
+    cout<<"Pointer 1 = "<<ptr<<endl;
+    cout<<"value 1 = "<<*ptr<<endl;
+    cout<<"Pointer 2 = "<<ptr<<endl;
+    cout<<"value 2 = "<<*ptr<<endl;
+    cout<<"ref = "<<&ref<<endl;    
+}
+int rose(int n){
+    if (n<=0){
+        return 1;
+    }
+    else{
+        return (rose(n-1)*n);
+    }
+}
+
